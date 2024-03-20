bede67888b363a138342da3d4675fef9eca7368a
Joel Brewer <joel@brewerdigital.com>
2024-03-19 21:38:48 -0600

    Finish exercise 1.5

---
+++ SICP/e1.5.scm
@@ -0,0 +1,22 @@
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+    0
+    y))
+
+
+
+(display (test 0 (p)))
+
+;; Normal-order
+;; (test 0 (p))
+;; (if (= 0 0)
+;;   0
+;;   p))
+;; Never has to evaluate p, because the logic stops before it gets there, so it returns 0
+
+;; Applicative-order
+;; Immediately evaluates and gets stuck in a loop:
+;; (test 0 (p))
+;; p gets evaluated to p, which gets evaluated to p, which...
