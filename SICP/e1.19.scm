e0171adc0732c7ba4f1b0e225dbd733b1a6f35c0
Joel Brewer <joel@brewerdigital.com>
2024-05-03 07:15:18 -0600

    WIP

---
+++ SICP/e1.19.scm
@@ -0,0 +1,22 @@
+;; (use-modules (ice-9 format))
+
+(define (fib n)
+  (display "a  b  p  q  count")
+  (newline)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (format #t "~d  ~d  ~d  ~d  ~d" a b p q count)
+  (newline)
+  (cond ((= count 0) a)
+	((even? count)
+	 (fib-iter a
+		   b
+		   1 ;; compute p'
+		   1 ;; compute q'
+		   (/ count 2)))
+	(else (fib-iter (+ (* b q) (* a q) (* a p))
+			(+ (* b p) (* a q))
+			p
+			q
+			(- count 1)))))
